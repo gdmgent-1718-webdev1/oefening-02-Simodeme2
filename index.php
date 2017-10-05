a8c106922c5d5a991bf46b71df684f3ab92b6821
Simodeme2 <Simodeme2@student.arteveldehs.be>
2017-10-05 10:52:28 +0200

    Update Git Reponsitory

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Oefening 1</title>
+</head>
+
+<body>
+<?php
+    $hello = "Hello";
+    $world = "World";
+
+    echo "<p>$hello $world</p>";
+?>
+</body>
+</html>
